5b73c6e20aa1d19eb13a9fb88ded1615fcd8f3e8
breathaning <etzhu1@gmail.com>
2026-02-02 09:53:51 -0800

    Fix typo in driver controller rotation deadband constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,7 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
     public static final double kDriverControllerVelocityDeadband = 0.1;
-    public static final double kDriveControllerRotationDeadband = 0.1;
+    public static final double kDriverControllerRotationDeadband = 0.1;
   }
 
   public static class DrivetrainConstants {
