fb24ee28f74a08e512fb3eb5ce56839dd059f96e
breathaning <etzhu1@gmail.com>
2026-02-02 21:33:42 -0800

    Refactor BirdAuto and PoseUtil classes; add FieldSetpointConstants and improve goal setpoint handling

--- src/main/java/frc/lib/BirdAuto.java
+++ src/main/java/frc/lib/BirdAuto.java
@@ -2,16 +2,37 @@ package frc.lib;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.function.Function;
+import java.util.stream.Stream;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class BirdAuto {
-    public static enum FieldSetpoint {
+    private static class FieldSetpointConstants {
+
+    }
+
+    private static enum FieldSetpoint {
+        OUTPOST(new Pose2d()),
+        DEPOT_LEFT(new Pose2d()),
+        DEPOT_RIGHT(new Pose2d()),
+        ALLIANCE_LEFT(new Pose2d()),
+        ALLIANCE_MIDDLE(new Pose2d()),
+        ALLIANCE_RIGHT(new Pose2d()),
+        NEUTRAL_LEFT_OUTER(new Pose2d()),
+        NEUTRAL_LEFT_INNER(new Pose2d()),
+        NEUTRAL_RIGHT_OUTER(new Pose2d()),
+        NEUTRAL_RIGHT_INNER(new Pose2d()),
+        TRENCH_LEFT_CLOSE(new Pose2d()),
+        TRENCH_LEFT_FAR(new Pose2d()),
+        TRENCH_RIGHT_CLOSE(new Pose2d()),
+        TRENCH_RIGHT_FAR(new Pose2d()),
+        CLIMB_RIGHT(new Pose2d()),
         CLIMB_LEFT(new Pose2d()),
         CLIMB_MIDDLE(new Pose2d()),
-        CLIMB_RIGHT(new Pose2d());
+        CLIMB_LEFT_ENTRY(new Pose2d()),
+        CLIMB_MIDDLE_ENTRY(new Pose2d()),
+        CLIMB_RIGHT_ENTRY(new Pose2d());
 
         public final Pose2d setpoint;
 
@@ -21,9 +42,12 @@ public class BirdAuto {
     }
 
     public static enum FieldGoal {
-        CLIMB_LEFT(new Goal(new Pose2d(),  lastSetpoint -> new Pose2d[] { FieldSetpoint.CLIMB_LEFT.setpoint })),
-        CLIMB_MIDDLE(new Goal(new Pose2d(), lastSetpoint -> new Pose2d[] { FieldSetpoint.CLIMB_LEFT.setpoint })),
-        CLIMB_RIGHT(new Goal(new Pose2d(), lastSetpoint -> new Pose2d[] { FieldSetpoint.CLIMB_LEFT.setpoint }));
+        CLIMB_LEFT(new Goal(new Pose2d(),
+                (previousSet) -> goalSetpointBuilder(FieldSetpoint.CLIMB_LEFT_ENTRY, FieldSetpoint.CLIMB_LEFT))),
+        CLIMB_MIDDLE(new Goal(new Pose2d(),
+                (lastSetpoint) -> goalSetpointBuilder(FieldSetpoint.CLIMB_RIGHT_ENTRY, FieldSetpoint.CLIMB_MIDDLE))),
+        CLIMB_RIGHT(new Goal(new Pose2d(),
+                (lastSetpoint) -> goalSetpointBuilder(FieldSetpoint.CLIMB_RIGHT_ENTRY, FieldSetpoint.CLIMB_RIGHT)));
 
         public final Goal goal;
 
@@ -32,7 +56,17 @@ public class BirdAuto {
         }
     }
 
-    public static record Goal(Pose2d pose, Function<Pose2d, Pose2d[]> setpointGenerator) {
+    private static Pose2d[] goalSetpointBuilder(FieldSetpoint... fieldSetpoints) {
+        return Stream.of(fieldSetpoints).map((fieldSetpoint) -> fieldSetpoint.setpoint).toArray(Pose2d[]::new);
+    }
+
+    @FunctionalInterface
+    // for autofilling
+    private interface SetpointGenerator {
+        Pose2d[] generate(Pose2d previousSetpoint);
+    }
+
+    private static record Goal(Pose2d pose, SetpointGenerator setpointGenerator) {
     }
 
     private final Pose2d initialSetpoint;
@@ -47,7 +81,9 @@ public class BirdAuto {
     }
 
     public void followPath(Alliance alliance) {
-        if (pathFinished) { return; }
+        if (pathFinished) {
+            return;
+        }
         boolean reachedLastSetpoint = false;
         if (reachedLastSetpoint) {
             pathProgress++;
@@ -57,6 +93,10 @@ public class BirdAuto {
             }
         }
         // follow
+        Pose2d setpoint = setpoints.get(pathProgress);
+        if (alliance == Alliance.Red) {
+            setpoint = PoseUtil.flipPoseToOtherAlliance(setpoint);
+        }
     }
 
     public void reset() {
@@ -68,7 +108,8 @@ public class BirdAuto {
         ArrayList<Pose2d> setpoints = new ArrayList<>();
         Pose2d lastSetpoint = null;
         for (Goal goal : goals) {
-            Pose2d[] generatedSetpoints = goal.setpointGenerator.apply(lastSetpoint != null ? lastSetpoint : initialSetpoint);
+            Pose2d[] generatedSetpoints = goal.setpointGenerator
+                    .generate(lastSetpoint != null ? lastSetpoint : initialSetpoint);
             lastSetpoint = generatedSetpoints[generatedSetpoints.length - 1];
             Collections.addAll(setpoints, generatedSetpoints);
         }
--- src/main/java/frc/lib/PoseUtil.java
+++ src/main/java/frc/lib/PoseUtil.java
@@ -3,13 +3,33 @@ package frc.lib;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public final class PoseUtil {
-    public static final Pose2d fieldFieldRelativePose(Pose2d pose) {
+    private static enum FieldZones {
+        FIELD(Units.inchesToMeters(651.22), Units.inchesToMeters(317.69)),
+        ALLIANCE(Units.inchesToMeters(156.61), Units.inchesToMeters(317.69));
+
+        public final double width, height;
+
+        private FieldZones(double width, double height) {
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    public static final Pose2d flipPoseToOtherAlliance(Pose2d pose) {
         return new Pose2d(
-                Units.inchesToMeters(651.22) - pose.getX(),
-                Units.inchesToMeters(317.69) - pose.getY(),
+                FieldZones.FIELD.width - pose.getX(),
+                FieldZones.FIELD.height - pose.getY(),
                 pose.getRotation().plus(new Rotation2d(Math.PI))
         );
+    }
+
+    public static final boolean isPoseInAllianceZone(Alliance alliance, Pose2d pose) {
+        if (alliance == Alliance.Red) {
+            pose = flipPoseToOtherAlliance(pose);
+        }
+        return pose.getX() <= FieldZones.ALLIANCE.width;
     }
 }
