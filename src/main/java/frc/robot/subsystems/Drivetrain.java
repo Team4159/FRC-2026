5fff93137d843fee235e9797562d00c47535698a
breathaning <etzhu1@gmail.com>
2026-02-07 16:53:21 -0800

    Rename kDriverControllerXYDeadband to kDriverControllerTranslationDeadband and update getInput methods for translation input

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,40 +1,54 @@
 package frc.robot.subsystems;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.generated.CommandSwerveDrivetrain;
 import frc.robot.generated.TunerConstants;
 
 public class Drivetrain extends CommandSwerveDrivetrain {
-    private static final double kInputXYExponent = 2.0; 
+    private static final double kInputTranslationExponent = 2.0; 
     private static final double kInputRotationExponent = 2.0; 
 
-    private final CommandXboxController controller;
+    private final Supplier<Double> inputX;
+    private final Supplier<Double> inputY;
+    private final Supplier<Double> inputRotation;
 
     public Drivetrain(CommandXboxController controller) {
         super(TunerConstants.DrivetrainConstants, TunerConstants.FrontLeft, TunerConstants.FrontRight, TunerConstants.BackLeft, TunerConstants.BackRight);
-        this.controller = controller;
+        this.inputX = () -> -controller.getLeftY();
+        this.inputY = () -> -controller.getLeftX();
+        this.inputRotation = () -> -controller.getRightX();
+    }
+
+    /** @return the field relative translation input (-left joystick y input, -left joystick x input), from magnitude range -1 to 1. a deadzone is applied.*/
+    public Translation2d getInputTranslation() {
+        Translation2d rawInput = new Translation2d(inputX.get(), inputY.get());
+        Vector<N2> filteredInputVector = MathUtil.applyDeadband(rawInput.toVector(), OperatorConstants.kDriverControllerTranslationDeadband, 1);
+        return new Translation2d(filteredInputVector);
     }
 
     /** @return the field relative x input (-left joystick y input), from range -1 to 1. a deadzone and quadratic are applied for better control.*/
     public double getInputX() {
-        double rawInput = -controller.getLeftY();
-        double filteredInput = MathUtil.applyDeadband(Math.abs(rawInput), OperatorConstants.kDriverControllerXYDeadband, 1);
-        return Math.abs(Math.pow(filteredInput, kInputXYExponent)) * Math.signum(rawInput);
+        double input = getInputTranslation().getX();
+        return Math.abs(Math.pow(input, kInputTranslationExponent)) * Math.signum(input);
     }
 
     /** @return the field relative y input (-left joystick x input), from range -1 to 1. a deadzone and quadratic are applied for better control.*/
     public double getInputY() {
-        double rawInput = -controller.getLeftX();
-        double filteredInput = MathUtil.applyDeadband(Math.abs(rawInput), OperatorConstants.kDriverControllerXYDeadband, 1);
-        return Math.abs(Math.pow(filteredInput, kInputXYExponent)) * Math.signum(rawInput);
+        double input = getInputTranslation().getY();
+        return Math.abs(Math.pow(input, kInputTranslationExponent)) * Math.signum(input);
     }
 
     /** @return the field relative rotation input (-right joystick x), from range -1 to 1. a deadzone and quadratic are applied for better control.*/
     public double getInputRotation() {
-        double rawInput = -controller.getRightX();
-        double filteredInput = MathUtil.applyDeadband(Math.abs(rawInput), OperatorConstants.kDriverControllerXYDeadband, 1);
+        double rawInput = inputRotation.get();
+        double filteredInput = MathUtil.applyDeadband(Math.abs(rawInput), OperatorConstants.kDriverControllerTranslationDeadband, 1);
         return Math.abs(Math.pow(filteredInput, kInputRotationExponent)) * Math.signum(rawInput);
     }
 }
