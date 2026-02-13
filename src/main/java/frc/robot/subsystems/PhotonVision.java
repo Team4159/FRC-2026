cf00a23bbc401033778755081d8dea5edaa7a529
breathaning <etzhu1@gmail.com>
2026-02-12 17:25:22 -0800

    new stddev function

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -11,12 +11,15 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.PhotonVisionConstants;
 
 public class PhotonVision extends SubsystemBase{
     private Drivetrain drivetrain;
@@ -25,6 +28,8 @@ public class PhotonVision extends SubsystemBase{
     //TODO: add other cameras later once we know where they are on the robot
     private PhotonCamera leftShooterCam, rightShooterCam;
     private PhotonPoseEstimator leftShooterEstimator, rightShooterEstimator;
+    private Matrix<N3, N1> kSingleTagStdDevs = PhotonVisionConstants.kSingleTagStdDevs;
+    private Matrix<N3, N1> kMultiTagStdDevs = PhotonVisionConstants.kMultiTagStdDevs;
 
     public PhotonVision(Drivetrain drivetrain){
         this.drivetrain = drivetrain;
@@ -52,7 +57,7 @@ public class PhotonVision extends SubsystemBase{
             //check if estimate exists
             if(leftShooterEstimate.isPresent()){
                 //set standard deviation
-                drivetrain.setVisionMeasurementStdDevs(calculateEstimationStdDevs(leftShooterEstimate, leftShooterCamResult.targets));
+                drivetrain.setVisionMeasurementStdDevs(calculateEstimationStdDevs(leftShooterEstimate, leftShooterCamResult.targets, leftShooterEstimator));
                 //send the pose estimate to the pose estimator
                 drivetrain.addVisionMeasurement(leftShooterEstimate.get().estimatedPose.toPose2d(), leftShooterEstimate.get().timestampSeconds);
             }
@@ -72,73 +77,74 @@ public class PhotonVision extends SubsystemBase{
             //check if estimate exists
             if(rightShooterEstimate.isPresent()){
                 //set standard deviation
-                drivetrain.setVisionMeasurementStdDevs(calculateEstimationStdDevs(rightShooterEstimate, rightShooterCamResult.targets));
+                drivetrain.setVisionMeasurementStdDevs(calculateEstimationStdDevs(rightShooterEstimate, rightShooterCamResult.targets, rightShooterEstimator));
                 //send the pose estimate to the pose estimator
                 drivetrain.addVisionMeasurement(rightShooterEstimate.get().estimatedPose.toPose2d(), rightShooterEstimate.get().timestampSeconds);
             }
         }
     }
 
-    //default photonvision stddev calculator
     // private Vector<N3> calculateEstimationStdDevs(
-    //         Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets, PhotonPoseEstimator photonEstimator) {
-    //         Vector<N3> stddevs;
-    //     if (estimatedPose.isEmpty()) {
-    //         // No pose input. Default to single-tag std devs
-    //         stddevs = kSingleTagStdDevs;
-
-    //     } else {
-    //         // Pose present. Start running Heuristic
-    //         var estStdDevs = kSingleTagStdDevs;
-    //         int numTags = 0;
-    //         double avgDist = 0;
-
-    //         // Precalculation - see how many tags we found, and calculate an average-distance metric
-    //         for (var tgt : targets) {
-    //             var tagPose = photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
-    //             if (tagPose.isEmpty()) continue;
-    //             numTags++;
-    //             avgDist +=
-    //                     tagPose
-    //                             .get()
-    //                             .toPose2d()
-    //                             .getTranslation()
-    //                             .getDistance(estimatedPose.get().estimatedPose.toPose2d().getTranslation());
+    //     Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets) {
+    //     //range should be form 0(no tag) to 1(full coverage) (limelight standard)
+    //     //photonvision area is scaled from 0-100 so need to convert
+    //     double area = 0;
+    //     //calculate area of all targets
+    //     if(estimatedPose.isPresent()){
+    //         for(var tag : targets){
+    //             //convert the scaling
+    //             area += tag.area/100; 
     //         }
-
-    //         if (numTags == 0) {
-    //             // No tags visible. Default to single-tag std devs
-    //             stddevs = kSingleTagStdDevs;
-    //         } else {
-    //             // One or more tags visible, run the full heuristic.
-    //             avgDist /= numTags;
-    //             // Decrease std devs if multiple targets are visible
-    //             if (numTags > 1) estStdDevs = kMultiTagStdDevs;
-    //             // Increase std devs based on (average) distance
-    //             if (numTags == 1 && avgDist > 4)
-    //                 estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
-    //             else estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
-    //             stddevs = estStdDevs;
-    //         }
+    //         System.out.println("stddev position: " + (1 - area * 0.3));
+    //         //TODO: tune, currently this is just the limelight one(why are sds on limelight negative lol)
+    //         //return VecBuilder.fill(1 - area * 0.3, 1 - area * 0.3, 1-area * 0.1);
+    //         return VecBuilder.fill(-1, -1, -1);
+    //     }else{
+    //         System.out.println("cooked :(");
     //     }
-    //     return stddevs;
+    //     //if the estimated pose does not exist just return extremely high stddevs
+    //     return VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
     // }
 
-    private Vector<N3> calculateEstimationStdDevs(
-        Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets) {
-        //range should be form 0(no tag) to 1(full coverage) (limelight standard)
-        //photonvision area is scaled from 0-100 so need to convert
-        double area = 0;
-        //calculate area of all targets
-        if(estimatedPose.isPresent()){
-            for(var tag : targets){
-                //convert the scaling
-                area += tag.area/100; 
+    private Matrix<N3, N1> calculateEstimationStdDevs(
+            Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets, PhotonPoseEstimator photonEstimator) {
+        if (estimatedPose.isEmpty()) {
+            // No pose input. Default to single-tag std devs
+            return kSingleTagStdDevs;
+
+        } else {
+            // Pose present. Start running Heuristic
+            var estStdDevs = kSingleTagStdDevs;
+            int numTags = 0;
+            double avgDist = 0;
+
+            // Precalculation - see how many tags we found, and calculate an average-distance metric
+            for (var tgt : targets) {
+                var tagPose = photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
+                if (tagPose.isEmpty()) continue;
+                numTags++;
+                avgDist +=
+                        tagPose
+                                .get()
+                                .toPose2d()
+                                .getTranslation()
+                                .getDistance(estimatedPose.get().estimatedPose.toPose2d().getTranslation());
             }
-            //TODO: tune, currently this is just the limelight one(why are sds on limelight negative lol)
-            return VecBuilder.fill(1 - area * 0.3, 1 - area * 0.3, 1-area * 0.1);
+
+            if (numTags == 0) {
+                // No tags visible. Default to single-tag std devs
+                return kSingleTagStdDevs;
+            } else {
+                // One or more tags visible, run the full heuristic.
+                avgDist /= numTags;
+                // Decrease std devs if multiple targets are visible
+                if (numTags > 1) estStdDevs = kMultiTagStdDevs;
+                // Increase std devs based on (average) distance
+                if (numTags == 1 && avgDist > 4)
+                    estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+                else estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
+                return estStdDevs;
+            }
         }
-        //if the estimated pose does not exist just return extremely high stddevs
-        return VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
     }
 }
