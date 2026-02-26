af0575f0459bd770c5a310660f0c04370795a334
bird <bird@bird.com>
2026-02-25 19:59:22 -0800

    ids

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -17,6 +17,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -67,9 +68,14 @@ public class Shooter extends SubsystemBase {
         rightBottomMotor.getConfigurator().apply(rightOutputConfigs);
     }
 
-    private final TalonFX kickerMotor = new TalonFX(100);
+    private final TalonFX kickerMotor = new TalonFX(20);
     {
         kickerMotor.getConfigurator().apply(new MotorOutputConfigs().withInverted(InvertedValue.CounterClockwise_Positive));
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Shooter Velocity", leftBottomMotor.getVelocity(true).getValue().in(RPM));
     }
 
     public Shooter() {
