77560f01ef3c9533b785faa38b4535bd1fa562a0
breathaning <etzhu1@gmail.com>
2026-03-08 00:26:34 -0800

    revert intake to voltage control and convert shooter pid units

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -287,7 +287,7 @@ public final class Constants {
     }};
 
     //Motor Config and PID
-    public static final double kP = 0.01;
+    public static final double kP = 0.01 / 12.0;
     public static final double kI = 0;
     public static final double kD = 0;
     public static final int ShooterIDOne = 9;
