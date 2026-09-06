3349908d36be8a6aef500e1e5a74adac69fde651
breathaning <etzhu1@gmail.com>
2026-09-05 17:41:59 -0700

    refactor: remove unused import

--- src/main/java/frc/lib/Telemetry.java
+++ src/main/java/frc/lib/Telemetry.java
@@ -27,7 +27,6 @@ import frc.robot.Constants.DrivetrainConstants;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
